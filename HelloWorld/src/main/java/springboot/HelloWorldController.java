179f27a544aa6b95a38502d37719412f2c34672c
nopains <nopains@126.com>
2020-01-08 07:08:18 +0800

    spring boot helloworld ready

---
+++ HelloWorld/src/main/java/springboot/HelloWorldController.java
@@ -0,0 +1,21 @@
+package springboot;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author manoj.bardhan
+ *
+ */
+@Controller
+@EnableAutoConfiguration
+public class HelloWorldController {
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello World Developer!!!";
+    }
+}
---
+++ demo/src/main/java/org/example/internal/SpringBootApplicationLuncher.java
@@ -0,0 +1,15 @@
+package org.example.internal;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * This Luncher for the spring boot application.
+ *
+ * @author manoj.bardhan
+ *
+ */
+public class SpringBootApplicationLuncher {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldController.class, args);
+    }
+}
