77127345aa5ff1de4a8db591ae9465d6902e722a
nopains <nopains@126.com>
2020-07-04 17:03:05 +0800

    add batch get

--- HelloWorld/src/main/java/springboot/HelloWorldController.java
+++ HelloWorld/src/main/java/springboot/HelloWorldController.java
@@ -3,7 +3,11 @@ package springboot;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 /**
  *
@@ -13,6 +17,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @EnableAutoConfiguration
 public class HelloWorldController {
+    private String keys;
+
     @RequestMapping("/hello")
     @ResponseBody
     public String sayHello() {
@@ -23,5 +29,20 @@ public class HelloWorldController {
     @ResponseBody
     public String auth() {
         return "auth check";
+    }
+
+    @RequestMapping("/batchGet")
+    @ResponseBody
+    public String batchGet(@RequestParam String keys) {
+        System.out.println(keys);
+
+        return keys;
+    }
+
+    @RequestMapping("/batchGetArray")
+    @ResponseBody
+    public String batchGetArray(@RequestParam ArrayList keys) {
+        System.out.println(keys);
+        return keys.toString();
     }
 }
--- HelloWorld/src/main/java/utils/StreamUtil.java
+++ HelloWorld/src/main/java/utils/StreamUtil.java
@@ -5,24 +5,19 @@ import java.util.stream.Stream;
 
 
 class StreamUtil {
-    private String name = "Jack";
     public static void main(String args[]) {
         HashMap<String,String> configMap = new HashMap<String,String>();
-        HashMap<String,String> resultMap = new HashMap<String,String>();
-        configMap.put("treeDepth", "12");
-        configMap.put("commentDepth", "13");
-        configMap.put("securityLink", "14");
-        String[] keys = (new String[]{"treeDepth", "commentDepth"});
+        configMap.put("config1", "12");
+        configMap.put("config2", "13");
+        configMap.put("config3", "14");
+        String[] keys = (new String[]{"config1", "config3"});
 //        String[] keys = (new String[]{"commentDepth"});
 //        String[] keys = (new String[]{"treeDepth", "securityLink"});
-
         Stream keysStream = Stream.of(keys);
+        HashMap<String,String> resultMap = new HashMap<String,String>();
         keysStream.filter((key) -> configMap.containsKey(key)).forEach( (key) -> {
             resultMap.put((String) key,configMap.get((String) key));
         });
-
-
         System.out.println(resultMap);
-        PrivateChild privateChildInstance = new PrivateChild();
     }
 }
