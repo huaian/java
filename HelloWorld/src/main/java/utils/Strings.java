c4da05cd509f0267a4c9651946b60ad5d192f38e
nopains <nopains@126.com>
2020-07-04 15:29:17 +0800

    add split demo

--- HelloWorld/src/main/java/utils/Strings.java
+++ HelloWorld/src/main/java/utils/Strings.java
@@ -1,11 +1,18 @@
 package utils;
 
+import java.util.stream.Stream;
+
 public class Strings {
 	public static void main(String args[]){
 		String testString = "testString测试字符串";
 		for (int i = 0; i < testString.length(); i++) {
 			System.out.println(testString.charAt(i));
 		}
+		String incomeString = "tom,jack,amily";
+		String[] incomeKeys = incomeString.split(",");
+		Stream.of(incomeKeys).forEach((key) -> {
+			System.out.println(key);
+		});
 	}
 
 }
